b6ed870244c49495551e081883bd9e5706580586
samuelantwi103 <76597396+samuelantwi103@users.noreply.github.com>
2024-09-04 16:28:46 +0000

    continuing material 3 design

--- microchek_app/lib/pages/home.dart
+++ microchek_app/lib/pages/home.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:microchek_app/pages/dashboard.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,7 +15,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return AdaptiveScaffold(
-      destinations: <NavigationDestination>[],
+      destinations: const <NavigationDestination>[
+        NavigationDestination(icon: Icon(Icons.home), label: "Background Check"),
+        NavigationDestination(icon: Icon(Icons.groups_rounded), label: "Persons"),
+        NavigationDestination(icon: Icon(Icons.settings), label: "My Institution"),
+        NavigationDestination(icon: Icon(Icons.logout), label: "Log Out"),
+
+      ],
+      smallBody: (context) => const GhanaCardValidationPage(),
     );
   }
 }
